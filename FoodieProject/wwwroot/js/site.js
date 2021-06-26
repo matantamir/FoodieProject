43af56d91d273938b96b7907020c8f6621de04e6
lotansery <Lotansery7@gmail.com>
2021-06-26 15:15:29 +0300

    Addresses view pages are clean + graphs script moved to site.js

--- FoodieProject/wwwroot/js/site.js
+++ FoodieProject/wwwroot/js/site.js
@@ -146,3 +146,123 @@ function aboutToggle() {
         }
     })
 }
+
+// Addresses Graphs ---------------------------------------------------------------------------------
+function responsivefy(svg) {
+    // get container + svg aspect ratio
+    var container = d3.select(svg.node().parentNode),
+        width = parseInt(svg.style("width")),
+        height = parseInt(svg.style("height")),
+        aspect = width / height;
+
+    // add viewBox and preserveAspectRatio properties,
+    // and call resize so that svg resizes on inital page load
+    svg.attr("viewBox", "0 0 " + width + " " + height)
+        .attr("preserveAspectRatio", "xMinYMid")
+        .call(resize);
+
+    // to register multiple listeners for same event type,
+    // you need to add namespace, i.e., 'click.foo'
+    // necessary if you call invoke this function for multiple svgs
+    // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+    d3.select(window).on("resize." + container.attr("id"), resize);
+
+    // get width of container and resize svg to fit it
+    function resize() {
+        var targetWidth = parseInt(container.style("width"));
+        if (targetWidth < 700) {
+            svg.attr("width", targetWidth);
+            svg.attr("height", Math.round(targetWidth / aspect));
+        }
+    }
+
+}
+// Parse the Data
+
+function build(data, width, height, max, svg) {
+
+    // X axis
+    var x = d3.scaleBand()
+        .range([0, width])
+        .domain(data.map(function (d) { return d.name; }))
+        .padding(0.2);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+        .domain([0, max * 1.2])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // Bars
+    svg.selectAll("mybar")
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) { return x(d.name); })
+        .attr("y", function (d) { return y(d.value); })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) { return height - y(d.value); })
+        .attr("fill", "#F5F3F0")
+
+}
+
+function createGraphInput(tableName, nameIndex, valueIndex)
+{
+    var elements = [];
+    var table = document.getElementById(tableName);
+
+    for (var i = 1; i < table.rows.length; i++) {
+        var currCity = table.rows[i].cells[0].innerHTML;
+        var flag = false;
+        var pos = -1;
+
+
+        // Check if the city is already exists in the element object
+        for (var j = 0; j < elements.length; j++) {
+            if (elements[j].name == currCity && flag == false) {
+                pos = j;
+                flag = true;
+            }
+        }
+
+        if (pos != -1) {
+            elements[pos].value++
+        }
+        else {
+            elements.push({ "name": table.rows[i].cells[nameIndex].innerHTML, "value": valueIndex == null ? 1 : table.rows[i].cells[valueIndex].innerHTML });
+        }
+    }
+
+    // Get maximum and minumum value
+    var min = 999
+    var max = 0
+    for (var i = 0; i < elements.length; i++) {
+        if (parseInt(elements[i].value) > parseInt(max)) { max = parseInt(elements[i].value); }
+        if (parseInt(elements[i].value) < parseInt(min)) { min = parseInt(elements[i].value); }
+    }
+
+    // set the dimensions and margins of the graph
+    var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+        width = 600 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#Area")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .call(responsivefy)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    build(elements,width,height,max,svg);
+}
